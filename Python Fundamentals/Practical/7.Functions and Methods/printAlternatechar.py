5f8f01236ee11bcc4e4993d404d44be4dd8ab8f7
Jay <jpatel9003@gmail.com>
2025-07-30 13:15:47 +0530

    Update printAlternatechar.py

--- Python Fundamentals/Practical/7.Functions and Methods/printAlternatechar.py
+++ Python Fundamentals/Practical/7.Functions and Methods/printAlternatechar.py
@@ -1,8 +1,7 @@
 # Example: 9) Write a Python program to print every alternate character from the
 # string starting from index 1.
-
 def printString(name):
-    for i in name:
+    for i in name(1,len(name),2):
         print(i)
 
 firatName = "Jay"
